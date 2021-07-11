40d49356fd881ad00791e6ae458c53c18728465e
Conaxian <accounts2@jeniweb.eu>
2021-07-11 23:40:06 +0200

    Update discord.js to v13

--- osmium/bot.js
+++ osmium/bot.js
@@ -1,20 +1,37 @@
+"use strict";
+
 require("dotenv").config();
-
 const Discord = require("discord.js");
-const options = {
-  ws: {
-    intents: new Discord.Intents(Discord.Intents.ALL)
-  }
-};
-const client = new Discord.Client(options);
 
+const allIntents = [
+  "GUILDS",
+  "GUILD_MEMBERS",
+  "GUILD_BANS",
+  "GUILD_EMOJIS",
+  "GUILD_INTEGRATIONS",
+  "GUILD_WEBHOOKS",
+  "GUILD_INVITES",
+  "GUILD_VOICE_STATES",
+  "GUILD_PRESENCES",
+  "GUILD_MESSAGES",
+  "GUILD_MESSAGE_REACTIONS",
+  "GUILD_MESSAGE_TYPING",
+  "DIRECT_MESSAGES",
+  "DIRECT_MESSAGE_REACTIONS",
+  "DIRECT_MESSAGE_TYPING"
+];
+const options = {
+  intents: new Discord.Intents(allIntents)
+};
+
+const client = new Discord.Client(options);
 const {DISCORD_TOKEN: TOKEN} = process.env
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on("message", async msg => {
+client.on("messageCreate", async msg => {
   if (msg.content === "Test") {
     await msg.reply("Successful test");
   }
