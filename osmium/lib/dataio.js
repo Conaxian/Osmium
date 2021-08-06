31608a3132f445954ea74bebf0886bdc668b8ff4
Conaxian <accounts2@jeniweb.eu>
2021-08-06 16:05:59 +0200

    Improve data reading and writing

--- osmium/lib/dataio.js
+++ osmium/lib/dataio.js
@@ -3,19 +3,27 @@
 const fs = require("fs/promises");
 const getPath = file => `data/${file}.json`;
 
+const cache = new Map();
+
 module.exports = exports = {
   async read(file) {
+    if (cache.has(file)) return cache.get(file);
     const path = getPath(file);
-    let json;
+    let object;
+
     try {
-      json = await fs.readFile(path, {encoding: "utf8"});
+      const json = await fs.readFile(path, {encoding: "utf8"});
+      object = JSON.parse(json);
     } catch (err) {
-      return {};
+      object = {};
     }
-    return JSON.parse(json);
+
+    cache.set(file, object);
+    return object;
   },
 
   async write(file, object) {
+    cache.set(file, object);
     const path = getPath(file);
     const json = JSON.stringify(object, null, 2);
     await fs.writeFile(path, json, {encoding: "utf8"});
