4bf52df04159f988eeb891aeb0f16795dbc69a1d
Conaxian <accounts2@jeniweb.eu>
2021-08-10 16:59:59 +0200

    Fix vanity invite

--- osmium/mods/info/cmd/invite.js
+++ osmium/mods/info/cmd/invite.js
@@ -10,11 +10,11 @@ module.exports = exports = {
   async *invoke(ctx) {
     const user = ctx.author?.user ?? ctx.author;
     const invite = ctx.guild.vanityURLCode ?
-      INVITE_URL + ctx.guild.vanityURLCode:
-      await ctx.channel.createInvite({
+      INVITE_URL + ctx.guild.vanityURLCode :
+      (await ctx.channel.createInvite({
         maxAge: 0,
         reason: `Created by ${user.tag}`
-      });
-    ctx.resolve({text: invite.url});
+      })).url;
+    ctx.resolve({text: invite});
   }
 };
